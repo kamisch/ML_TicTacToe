288abb382545103cc2fba11b37d2fb59e6d6a920
kamisch <chengtwv2@gmail.com>
2019-03-27 12:06:38 -0400

    minor mod

--- game.py
+++ game.py
@@ -7,4 +7,14 @@ class Game:
         self.player1 = player1
         self.player2 = player2
         self.winner = None
-
+        self.starter = None
+        self.challenger = None
+    def start(self):
+        whoStarts = np.random.randint(2, size=1)
+        if whoStarts[0] == 0:
+            self.starter = self.player1
+            self.challenger = self.player2
+        else:
+            self.starter = self.player2
+            self.challenger = self.player1
+    
--- player.py
+++ player.py
@@ -1,1 +1,9 @@
 #agent
+class Player:
+    def __init__(self,board):
+        self.nextMove = None
+        self.board = board
+    def move(self,board):
+        self.board = board
+        
+        
