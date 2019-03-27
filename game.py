612603e3dca3cef5711ff3ec005a910ae2d48ec7
kamisch <chengtwv2@gmail.com>
2019-03-27 10:58:16 -0400

    added game class

--- game.py
+++ game.py
@@ -1,1 +1,10 @@
-#tic tac toe main gain boy
+#tic tac toe main
+import numpy as np
+class Game:
+    def __init__(self,player1,player2):
+        self.board = np.matrix('e e e; e e e; e e e')
+        self.move = 0
+        self.player1 = player1
+        self.player2 = player2
+        self.winner = None
+
